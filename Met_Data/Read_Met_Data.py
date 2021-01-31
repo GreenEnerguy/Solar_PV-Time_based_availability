93cd1100082786fad7d05c2bfcf7316996aeb9a1
GreenEnerguy <martinez.fanals@gmail.com>
2021-01-31 00:31:23 -0600

    Updated comments explaining the script

--- Met_Data/Read_Met_Data.py
+++ Met_Data/Read_Met_Data.py
@@ -11,19 +11,22 @@ def read_met_data(met_data_path, timeframe):
     '''
     Parameters
     ----------
-    met_data_path : String name indicating the relative path where the meteorological data is located 
-                    e.g. 'Met_Data' is the subfolder from current folder where to look for this data
-    timeframe : String name indicating the folder where the meteorological csv data to be read is located;
-                e.g. the subfolder \Met_Data\2020_09 contains the csv file where the September meteo data is
-                located. timeframe refers to '2020_09'
+    met_data_path : String name indicating the relative path of the folder where the meteorological data is located 
+                    e.g. 'Met_Data' is the subfolder relative to the folder containing "Availability_Calc.py" where 
+                    to look for this data
+    timeframe : String name indicating the folder with the meteorological csv data to be read; e.g. the subfolder 
+                \Met_Data\2020_09\ contains the csv file where the September meteo data is stored. In this instance, 
+                timeframe refers to '2020_09'
 
     Returns
     -------
-    meteo : Pandas Dataframe containing the clean meteorological data read in the csv file located in 
-            \met_data_path\timeframe, which index is day and time
+    meteo : Pandas Dataframe that contain the clean meteorological data read in the csv file located in 
+            \met_data_path\timeframe, the index being day and time
 
     '''
     
+    # In my specific case, the met data is stored in the timeframe folder within a .csv file 
+    # that includes the string 'Report' in the file name
     path = os.path.join(os.getcwd(), met_data_path, timeframe)       
     for file in os.listdir(path):
         if file.endswith('csv') and 'Report' in file:
@@ -32,10 +35,9 @@ def read_met_data(met_data_path, timeframe):
     file_name = os.path.join(path, met_data)
     project_name = pd.read_csv(file_name).columns[0].split(';')[-1].replace('"', '')
     
-
+    # In the following chunk of code I conduct a cleaning of the csv file
     meteo = pd.read_csv(file_name, engine='python', delimiter='\;', skiprows=2)
     meteo = meteo.applymap(lambda x: x.replace('"', '')) # replace " from all the dataframe
-    # meteo.columns = met_col_names
     meteo.columns = ['RAD_' + str(i) + ' [W/m2]' for i in range(len(meteo.columns))]
     
 
@@ -60,16 +62,13 @@ def read_met_data(met_data_path, timeframe):
     del meteo[meteo.columns[0]] 
     meteo.drop(meteo.columns[num_columns-1:], inplace=True, axis=1)
     
-    start_date = meteo.index.values.astype(str)[0][:16] \
-                                .replace('T', '-').replace(':', 'h') # get first measurement item
-    end_date = meteo.index.values.astype(str)[-1][:16] \
-                                .replace('T', '-').replace(':', 'h') # get last measurement item
+    # start_date = meteo.index.values.astype(str)[0][:16] \
+    #                             .replace('T', '-').replace(':', 'h') # get first measurement item
+    # end_date = meteo.index.values.astype(str)[-1][:16] \
+    #                            .replace('T', '-').replace(':', 'h') # get last measurement item
     
     # create name of the .csv file as a string
-    csv_name = os.getcwd() + '\\' + project_name + '_' + start_date + '_' + end_date + '.csv'
-    
-    # export dataframe to a .csv file
-    # meteo.to_csv(csv_name, index=True)
+    # csv_name = os.getcwd() + '\\' + project_name + '_' + start_date + '_' + end_date + '.csv'
     
     return meteo
         
